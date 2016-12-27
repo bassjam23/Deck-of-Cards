f0081b28605530ac66ab90c195bb15cea5b12527
Cameron Smith <spellbee23@camerons-macbook-pro.local>
2016-12-26 18:59:10 -0700

    Initial Commit

---
+++ Deck of Cards/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Deck of Cards
+//
+//  Created by Cameron Smith on 12/26/16.
+//  Copyright © 2016 Cameron Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
